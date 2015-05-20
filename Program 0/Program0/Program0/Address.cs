1efd4c56504b5a233f246a8900776357c1098976
Nick <njwolf64@gmail.com>
2015-05-20 19:01:05 -0400

    Add more to adress class

--- Program 0/Program0/Program0/Address.cs
+++ Program 0/Program0/Program0/Address.cs
@@ -8,10 +8,24 @@ namespace Program0
 {
     class Address
     {
+        //read only
         public string AddressOne { get; private set; }
+        //read only
         public string AddressTwo { get; private set; }
+        //read only
         public string City { get; private set; }
+        //ready only
         public string State { get; private set; }
-        private int ZipCode;
+        //32 bit 0 to 4,294,967,295, inclusive
+        private uint zipCode;
+        public Address(string addressOne, string addressTwo, string city, string state, uint zip)
+        {
+            AddressOne = addressOne;
+            AddressTwo = addressTwo;
+            City = city;
+            State = state;
+
+        }
+        
     }
 }
